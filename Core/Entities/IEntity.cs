c0f5d32dc1f281185014f220e6a1125ef16beea1
Irem Bulut <73583655+bulutirm@users.noreply.github.com>
2021-02-12 17:59:36 +0300

    Add files via upload

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    //IEntity implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal: IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
+
+//Code Refactoring
